aa986c8682eef1954f2b734c91e81a9092ac4cae
DevStarlight <jesushuertaarrabal@gmail.com>
2018-09-13 21:45:58 +0200

    Increase the amount of time the user can pick a dish (30 min before the schedule end, the dish will not be available) Setup global variables

--- src/providers/global-provider.ts
+++ src/providers/global-provider.ts
@@ -3,15 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalProvider {
 
-	public serverURL: String = "http://backend:3000/"; // Development
+	// public serverURL: String = "http://backend:3000/"; // Development
 	// public serverURL: String = "https://beta.sweetsuomi.com/api/v1/"; // Beta
-	// private serverURL: String = "https://api.sweetsuomi.com/"; // Production
-	public s3Url: String = "https://s3-eu-west-1.amazonaws.com/beta.resources.sweetsuomi.com/"; // Development & Beta
-	// public s3Url: String = "https://s3-eu-west-1.amazonaws.com/resources.sweetsuomi.com/"; // Production
+	private serverURL: String = "https://api.sweetsuomi.com/api/v1/"; // Production
+	// public s3Url: String = "https://s3-eu-west-1.amazonaws.com/beta.resources.sweetsuomi.com/"; // Development & Beta
+	public s3Url: String = "https://s3-eu-west-1.amazonaws.com/resources.sweetsuomi.com/"; // Production
 	public cloudFrontURL: String = "https://d3ksovztvyvzlz.cloudfront.net/"; // Development & Beta
 	// public cloudFrontURL: String = "https://s3-eu-west-1.amazonaws.com/beta.resources.sweetsuomi.com/"; // Production
 
-	public removeDishFromMenu = 15;
+	public removeDishFromMenu = 30;
 
 	constructor() { }
 }
