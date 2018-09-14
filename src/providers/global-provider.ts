fec37fc77ce749bbf7e2efe944b0d48c0f727b4c
DevStarlight <jesushuertaarrabal@gmail.com>
2018-09-14 22:56:18 +0200

    Fixed an issue with iOS icon sizes Fixed an issue with public serverURL

--- src/providers/global-provider.ts
+++ src/providers/global-provider.ts
@@ -5,7 +5,7 @@ export class GlobalProvider {
 
 	// public serverURL: String = "http://backend:3000/"; // Development
 	// public serverURL: String = "https://beta.sweetsuomi.com/api/v1/"; // Beta
-	private serverURL: String = "https://api.sweetsuomi.com/api/v1/"; // Production
+	public serverURL: String = "https://api.sweetsuomi.com/api/v1/"; // Production
 	// public s3Url: String = "https://s3-eu-west-1.amazonaws.com/beta.resources.sweetsuomi.com/"; // Development & Beta
 	public s3Url: String = "https://s3-eu-west-1.amazonaws.com/resources.sweetsuomi.com/"; // Production
 	public cloudFrontURL: String = "https://d3ksovztvyvzlz.cloudfront.net/"; // Development & Beta
